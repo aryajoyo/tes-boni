8b86d1567f8304f619af50443ae68c505c057b38
Arya Joyo <57989547+aryajoyo@users.noreply.github.com>
2025-01-11 22:27:51 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProdukController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+
+
+Route::get('/produk', [ProdukController::class, 'index'])->name('produk.index');
+Route::get('/produk/create', [ProdukController::class, 'create'])->name('produk.create');
+Route::post('/produk', [ProdukController::class, 'store'])->name('produk.store');
+Route::get('/produk/{produk}/edit', [ProdukController::class, 'edit'])->name('produk.edit');
+Route::put('/produk/{produk}', [ProdukController::class, 'update'])->name('produk.update');
+Route::delete('/produk/{produk}', [ProdukController::class, 'destroy'])->name('produk.destroy');
+
+
+
+
+
+
+
+
+Route::get('/fetch-produk', [ProdukController::class, 'fetchProduk']);
